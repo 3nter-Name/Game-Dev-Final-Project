e3caa5c421d395fef3292f5af92b3de674e62a52
Enter-Name <Enter-Name@outlook.com>
2026-05-09 19:13:24 -0400

    Stuff 4

--- Levels/main_controller.gd
+++ Levels/main_controller.gd
@@ -1,8 +1,11 @@
 extends Node
 
+var room_count;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	room_count = 1;
+	
 	var first_room = preload("res://Levels/level_1.tscn");
 	var room = first_room.instantiate();
 	$Current_Room.add_child(room);
@@ -16,11 +19,15 @@ func _process(delta: float) -> void:
 
 
 func move_on() -> void:
-	var next_room = preload("res://Levels/level_2.tscn");
-	var room = next_room.instantiate();
-	$Current_Room.get_child(0).queue_free();
-	$Current_Room.add_child(room);
-	room.init($Player);
+	if room_count == 1:
+		var next_room = preload("res://Levels/level_2.tscn");
+		var room = next_room.instantiate();
+		$Current_Room.get_child(0).queue_free();
+		$Current_Room.add_child(room);
+		room.init($Player);
+		room_count += 1;
+	else:
+		success();
 
 func success() -> void:
 	get_tree().change_scene_to_file("res://Non-Game Scenes/Success.tscn");
